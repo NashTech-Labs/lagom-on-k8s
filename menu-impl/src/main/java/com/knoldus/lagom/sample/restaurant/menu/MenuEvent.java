bd520f8f9fb69955925df3d2c4852746db1ce781
Himanshu Gupta <himanshu@knoldus.com>
2018-10-14 18:54:30 +0530

    added menu event

---
+++ menu-impl/src/main/java/com/knoldus/lagom/sample/restaurant/menu/MenuEvent.java
@@ -0,0 +1,34 @@
+package com.knoldus.lagom.sample.restaurant.menu;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.lightbend.lagom.javadsl.persistence.AggregateEvent;
+import com.lightbend.lagom.javadsl.persistence.AggregateEventShards;
+import com.lightbend.lagom.javadsl.persistence.AggregateEventTag;
+import com.lightbend.lagom.javadsl.persistence.AggregateEventTagger;
+import com.lightbend.lagom.serialization.CompressedJsonable;
+import com.lightbend.lagom.serialization.Jsonable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+public interface MenuEvent extends Jsonable, AggregateEvent<MenuEvent> {
+
+    int NUM_SHARDS = 20; //TODO: Should be static and decided before deploying application on production
+
+    AggregateEventShards<MenuEvent> TAG = AggregateEventTag.sharded(MenuEvent.class, NUM_SHARDS);
+
+    @Override
+    default AggregateEventTagger<MenuEvent> aggregateTag() {
+        return TAG;
+    }
+
+    /**
+     * Event generated when an Item gets added to Menu.
+     */
+    @Builder
+    @JsonDeserialize
+    @AllArgsConstructor
+    final class ItemAdded implements MenuEvent, CompressedJsonable {
+        private final Item item;
+    }
+
+}
