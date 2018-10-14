e334d76ee09b356151cc2341175c7d3b2d740e9f
Himanshu Gupta <himanshu@knoldus.com>
2018-10-14 18:59:16 +0530

    added menu state

---
+++ menu-impl/src/main/java/com/knoldus/lagom/sample/restaurant/menu/MenuState.java
@@ -0,0 +1,15 @@
+package com.knoldus.lagom.sample.restaurant.menu;
+
+import com.lightbend.lagom.serialization.CompressedJsonable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public final class MenuState implements CompressedJsonable {
+    private final List<Item> menu;
+}
