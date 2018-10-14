6307c2f372d50810d76d08d7f4550e28f2699cb9
Himanshu Gupta <himanshu@knoldus.com>
2018-10-14 18:42:25 +0530

    added menu command

---
+++ menu-impl/src/main/java/com/knoldus/lagom/sample/restaurant/menu/MenuCommand.java
@@ -0,0 +1,19 @@
+package com.knoldus.lagom.sample.restaurant.menu;
+
+import akka.Done;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.lightbend.lagom.javadsl.persistence.PersistentEntity;
+import com.lightbend.lagom.serialization.Jsonable;
+import lombok.Builder;
+import lombok.Getter;
+
+public interface MenuCommand extends Jsonable {
+
+    @Getter
+    @Builder
+    @JsonDeserialize
+    final class AddItem implements MenuCommand, PersistentEntity.ReplyType<Done> {
+        private final Item item;
+    }
+
+}
