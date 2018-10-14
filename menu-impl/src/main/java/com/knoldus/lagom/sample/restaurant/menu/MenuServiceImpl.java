5c8f33bd9cd5bdd30b17d0b6eca9829aa140f051
Himanshu Gupta <himanshu@knoldus.com>
2018-10-14 18:35:58 +0530

    implementing service

---
+++ menu-impl/src/main/java/com/knoldus/lagom/sample/restaurant/menu/MenuServiceImpl.java
@@ -0,0 +1,33 @@
+package com.knoldus.lagom.sample.restaurant.menu;
+
+import akka.Done;
+import akka.NotUsed;
+import com.google.inject.Inject;
+import com.lightbend.lagom.javadsl.api.ServiceCall;
+import com.lightbend.lagom.javadsl.persistence.PersistentEntityRegistry;
+
+import java.util.List;
+
+public final class MenuServiceImpl implements MenuService {
+    private final PersistentEntityRegistry registry;
+
+    @Inject
+    public MenuServiceImpl(final PersistentEntityRegistry registry) {
+        this.registry = registry;
+    }
+
+    @Override
+    public ServiceCall<Item, Done> addItem() {
+        return null;
+    }
+
+    @Override
+    public ServiceCall<NotUsed, List<Item>> getMenu() {
+        return null;
+    }
+
+    @Override
+    public ServiceCall<Item, Done> deleteItem() {
+        return null;
+    }
+}
