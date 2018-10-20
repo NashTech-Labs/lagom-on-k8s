fa20600c6b6b29d028261ce3b00b89163e0c784d
Himanshu Gupta <himanshu@knoldus.com>
2018-10-20 19:36:46 +0530

    added support for kubernetes

--- menu-impl/src/main/java/com/knoldus/lagom/sample/restaurant/menu/impl/MenuModule.java
+++ menu-impl/src/main/java/com/knoldus/lagom/sample/restaurant/menu/impl/MenuModule.java
@@ -1,12 +1,41 @@
 package com.knoldus.lagom.sample.restaurant.menu.impl;
 
+import akka.actor.ActorSystem;
+import akka.management.AkkaManagement$;
+import akka.management.cluster.bootstrap.ClusterBootstrap$;
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
 import com.knoldus.lagom.sample.restaurant.menu.api.MenuService;
 import com.lightbend.lagom.javadsl.server.ServiceGuiceSupport;
+import com.typesafe.config.Config;
+import play.Application;
+import play.Environment;
 
 public final class MenuModule extends AbstractModule implements ServiceGuiceSupport {
+  private final Environment environment;
+  private final Config config;
+
+  public MenuModule(final Environment environment, final Config config) {
+    this.environment = environment;
+    this.config = config;
+  }
+
   @Override
   protected void configure() {
+    if (environment.isProd()) {
+      bind(AkkaManagerAndClusterStarter.class).asEagerSingleton();
+    }
+
     bindService(MenuService.class, MenuServiceImpl.class);
+  }
+
+  static class AkkaManagerAndClusterStarter {
+    @Inject
+    AkkaManagerAndClusterStarter(final Application application, final ActorSystem actorSystem) {
+      if (application.isProd()) {
+        AkkaManagement$.MODULE$.get(actorSystem).start();
+        ClusterBootstrap$.MODULE$.get(actorSystem).start();
+      }
+    }
   }
 }
